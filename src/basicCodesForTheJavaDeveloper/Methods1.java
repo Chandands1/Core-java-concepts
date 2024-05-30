38e4b4ccd1098835bccefeb031de339c13995780
chandansomashekar <chandan.somashekar@6dtech.co.in>
2024-05-30 14:35:37 +0530

    morecodes

---
+++ src/basicCodesForTheJavaDeveloper/Methods1.java
@@ -0,0 +1,14 @@
+package basicCodesForTheJavaDeveloper;
+
+public class Methods1 {
+	
+	public static void main(String[] args) {
+		
+		Methods1.test();
+	}
+	public static void test() {
+		
+		System.out.println("Again its me");
+	}
+
+}
---
+++ src/basicCodesForTheJavaDeveloper/MethodsInJava.java
@@ -0,0 +1,18 @@
+package basicCodesForTheJavaDeveloper;
+
+public class MethodsInJava {
+	
+	public static void main(String[] args) {
+		MethodsInJava a1 = new MethodsInJava();
+		a1.test();
+		
+		
+	}
+	
+	public void test() {
+		System.out.println("I am Chandan ");
+		
+		
+	}
+
+}
